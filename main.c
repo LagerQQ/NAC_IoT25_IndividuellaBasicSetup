e9c873483ea0a6419ef8c45a1b384d5c66f34d3c
Fredrik <tupplur_bloggarna.0w@icloud.com>
2026-02-23 00:13:03 +0100

    Added some functions to change from delay_ms.. to millis, amongst other things

--- main.c
+++ main.c
@@ -1,18 +1,61 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
+#include <stdint.h>
+#include <util/atomic.h>
+
+volatile uint32_t g_ms = 0;
+
+uint32_t millis(void) 
+{
+    uint32_t ms;
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) 
+    {
+        ms = g_ms;
+    }
+    return ms;
+}
+
+void timer0_init_ms(void) 
+{
+    TCNT0 = 0;
+    TCCR0A |= (1 << WGM01);
+    OCR0A = 249;
+    TCCR0B |= (1 << CS01) | (1 << CS00);
+    TIMSK0 |= (1 << OCIE0A);
+    sei();
+}
+
+ISR(TIMER0_COMPA_vect) 
+{
+    g_ms++;
+}
 
 int main(void)
 {
     // Pin 13 på Arduino Uno = PB5 på ATmega328P
     DDRB |= (1 << DDB5);   // Sätt PB5 som utgång
 
+    timer0_init_ms();
+
+    uint32_t lastToggle = 0;
+    uint16_t interval = 250;
+    uint8_t ledOn = 0;
+
     while (1)
     {
-        PORTB |= (1 << PORTB5);   // Tänd LED
-        _delay_ms(500);
+        uint32_t now = millis();
 
-        PORTB &= ~(1 << PORTB5);  // Släck LED
-        _delay_ms(500);
+        if (now - lastToggle >= interval) {
+            lastToggle += interval;
+            ledOn = !ledOn;
+            if(ledOn == 1) {
+                PORTB |= (1 << PORTB5);
+            }
+            else {
+                PORTB &= ~(1 << PORTB5);
+            }
+        }
     }
 
     return 0;
